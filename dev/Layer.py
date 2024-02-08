5bf99b6f5f91cf3d1ba333a5736eae24f234d77e
Anthony Micco <anthonymicco2003@gmail.com>
2024-02-08 13:40:14 -0500

    fixed get_distance_on/off method

--- dev/Layer.py
+++ dev/Layer.py
@@ -62,33 +62,15 @@ class Layer:
         """
         Indicates the total distance when the laser was ON
         """
-        # Define the shift, x_shift and y_shift columns that moves the index down 1
-        self.df["shift"] = self.df["laser_status"].shift()
-        self.df["x_shift"] = self.df["x"].shift()
-        self.df["y_shift"] = self.df["y"].shift()
-
-        # Define distance_off
+        # Define distance_on
         distance_on = 0
 
         # iterate through the data frame
         for index, rows in self.df.iterrows():
 
-            # Defines the old and new values
-            old = self.df["shift"][index]
-            new = self.df["laser_status"][index]
-
             # Determines if the laser is on
-            if (old == 1 and new == 1) or (old == 0 and new == 1):
-                # Determine the values of x and y by calculating the distance between the new point and old point
-                x = self.df["x"][index] - self.df["x_shift"][index]
-                y = self.df["y"][index] - self.df["y_shift"][index]
-
-                # Calculate the length between the two points (Pythag Theorem)
-                len = np.sqrt(x ** 2 + y ** 2)
-
-                # Add length to dataframe and add to the distance_on
-                self.df.loc[index, "length"] = len
-                distance_on += len
+            if self.df.loc[index, "laser_status"] == 1:
+                distance_on += self.df.loc[index, "length"]
 
         print(f"Distance the laser was on: {distance_on}")
 
@@ -96,35 +78,17 @@ class Layer:
         """
         Indicates the total distance when the laser was off
         """
-        # Define the shift, x_shift and y_shift columns that moves the index down 1
-        self.df["shift"] = self.df["laser_status"].shift()
-        self.df["x_shift"] = self.df["x"].shift()
-        self.df["y_shift"] = self.df["y"].shift()
-
         # Define distance_off
         distance_off = 0
 
         # iterate through the data frame
         for index, rows in self.df.iterrows():
 
-            # Defines the old and new values
-            old = self.df["shift"][index]
-            new = self.df["laser_status"][index]
+            # Determines if the laser is on
+            if self.df.loc[index, "laser_status"] == 0:
+                distance_off += self.df.loc[index, "length"]
 
-            # Determines if the laser is off
-            if (old == 0 and new == 0) or (old == 1 and new == 0):
-                # Determine the values of x and y by calculating the distance between the new point and old point
-                x = self.df["x"][index] - self.df["x_shift"][index]
-                y = self.df["y"][index] - self.df["y_shift"][index]
-
-                # Calculate the length between the two points (Pythag Theorem)
-                len = np.sqrt(x ** 2 + y ** 2)
-
-                # Add length to dataframe and add to the distance_off
-                self.df.loc[index, "length"] = len
-                distance_off += len
-
-        print(f"Distance the laser was off: {distance_off}")
+        print(f"Distance the laser was on: {distance_off}")
 
     def get_time_on(self):
         """
